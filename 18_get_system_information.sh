95ee9850adeaba00164a6ec9f2b988f732c9b6e8
Stanislav Olshanskyi <stasolsh77@gmail.ccom>
2024-08-12 21:16:45 +0300

    Add free and lscpu

--- 18_get_system_information.sh
+++ 18_get_system_information.sh
@@ -4,4 +4,3 @@ lsb_release -a           # Display Linux distribution information
 free -m                  # Display total amount of available memory in megabytes
 lscpu                    # Display detailed information about the CPU
 lsblk                    # Display the block devices available to a system
-lscpu                    # Display detailed information about the CPU
