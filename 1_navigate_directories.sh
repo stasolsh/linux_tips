dc4b765a2bf48b02e07256b8332aa9f3f9e12204
Stanislav Olshanskyi <stasolsh77@gmail.ccom>
2024-08-16 13:34:53 +0300

    Add pwd and mv commands

--- 1_navigate_directories.sh
+++ 1_navigate_directories.sh
@@ -3,3 +3,4 @@ cd -       # Switch to the previous directory
 cd ~       # Go to the home directory
 cd ..      # Go up one directory level
 cd ../..   # Go up two directory levels
+pwd        # Print working directory
--- 3_creating_and_removing_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-touch filename          # Create an empty file
-mkdir dirname           # Create a directory
-mkdir -p parent/child   # Create nested directories
-rm filename             # Remove a file
-rm -r dirname           # Remove a directory and its contents
